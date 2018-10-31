eeb232cbf8729a1008421c9457f34b202400008d
jenn <soothingjennyg@gmail.com>
2018-10-31 11:44:52 -0700

    random class

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,4 +1,13 @@
-public class List {
-    int [] randomList;
+public List<Interger> randomList(int n) {
+    ArrayList<Integer> list = new ArrayList<Inteeger>(n);
+    Random random = new Random();
 
+    for (int i = 0; i < n; ++i)
+        {
+            list.add(random.nextInt(1000));
+        }
+        return list;
 }
+
+
+
