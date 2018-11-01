0ebfa0fa9c609350d00f3fd889a7fa86fada645e
jenn <soothingjennyg@gmail.com>
2018-11-01 13:06:20 -0700

    did the sorts with arrayList, not tested.  should have unique values

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,13 +1,46 @@
-import java.util.Random;
+import java.util.*;
 
 public class List {
-    public void BackwardsList(int n){
-        int [] backwardsList = new int[n];
-        int count = n-1;
+    public void backwardsList(int n){
+        ArrayList<Integer> backwards = new ArrayList<Integer>();
         for (int i = 0; i < n; i++)
         {
-           backwardsList[i]= count;
-           --count;
+            backwards.add(n-i);
+        }
+        System.out.println("Backwards List" + backwards);
+    }
+
+    public void randomList(int size){
+        ArrayList<Integer> random = new ArrayList<Integer>();
+        for (int i = 0; i < size; ++i)
+        random.add(i);
+
+        System.out.println("Original List: " + random);
+        Collections.shuffle(random);
+        System.out.println("Shuffled List: " + random);
+    }
+
+
+
+    //This will not fill the list to the exact size of the list but to sizeParts *n < sizeList  TODO: Can be changed
+    public void almostSort(int sizeList, int sizeParts){
+        ArrayList<Integer> almostSort = new ArrayList<Integer>();
+        int whereAt = 0;
+
+        while (sizeList > sizeParts) {
+            ArrayList<Integer> toAdd = new ArrayList<Integer>();
+
+            for (int i = whereAt; i < whereAt + sizeParts; ++i) {
+                toAdd.add(i);
+            }
+
+            System.out.println("Original List: " + toAdd);
+            Collections.shuffle(toAdd);
+            System.out.println("Original List: " + toAdd);
+            almostSort.addAll(toAdd);
+            whereAt +=sizeParts;
+            System.out.println("Original List: " + almostSort);
+
         }
     }
 }
