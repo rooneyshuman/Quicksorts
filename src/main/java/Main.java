8c8974166e574100dd849b95b21dfb9059598e0d
rooneyshuman <rooneyshuman@users.noreply.github.com>
2018-11-11 18:16:15 -0800

    Added timer class to handle timing execution times and saving to file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,66 +7,67 @@ public class Main {
     QuickInsert insert = new QuickInsert();
     Quick3 dualPivot = new Quick3();
     long startTime, endTime, totalTime;
+    Timer timer = new Timer();
 
     // Backwards list sorted with regular quicksort
     List backList = new List(listSize);
     backList.backwardsList();         //generate backwards list
     System.out.println("Backwards List - Unsorted");
     backList.display();
-    startTime = System.nanoTime();
+    timer.start();
     quick.sort(backList, 0, backList.getSize() - 1);
-    endTime = System.nanoTime();
+    timer.stop();
     System.out.println("Backwards List - Sorted with Quick");
     backList.display();
     System.out.print("Sorting time: ");
-    totalTime = endTime - startTime;
-    System.out.println(totalTime);
+    System.out.println(timer.totalTime);
     System.out.println();
+    timer.saveTime("backListQuick.txt");
 
-    // Random list sorted with quickinsert
+    // Random list sorted with quickInsert
     List randomList = new List(listSize);
     randomList.randomList();        //generate random list
     System.out.println("Random List - Unsorted");
     randomList.display();
-    startTime = System.nanoTime();
+    timer.start();
     insert.sort(randomList, 0, randomList.getSize() - 1);
-    endTime = System.nanoTime();
+    timer.stop();
     System.out.println("Random List - Sorted with QuickInsert");
     randomList.display();
     System.out.print("Sorting time: ");
-    totalTime = endTime - startTime;
-    System.out.println(totalTime);
+    System.out.println(timer.totalTime);
     System.out.println();
+    timer.saveTime("randomListQuickInsert.txt");
 
-    // Almost sorted list sorted with regular quicksort
+    // Almost sorted list sorted with quickInsert
     List almostList = new List(listSize);
     almostList.almostSort(4);       //generate almost sorted list
     System.out.println("Almost Sorted List - Unsorted");
     almostList.display();
-    startTime = System.nanoTime();
+    timer.start();
     insert.sort(almostList, 0, almostList.getSize() - 1);
-    endTime = System.nanoTime();
+    timer.stop();
     System.out.println("Almost Sorted List - Sorted with QuickInsert");
     almostList.display();
     System.out.print("Sorting time: ");
-    totalTime = endTime - startTime;
-    System.out.println(totalTime);
+    System.out.println(timer.totalTime);
     System.out.println();
+    timer.saveTime("almostSortedQuickInsert.txt");
 
     //Backwards list sorted with dual pivot quicksort
     List backList2 = new List(listSize);
     backList2.backwardsList();         //generate backwards list
     System.out.println("Backwards List - Unsorted");
     backList2.display();
-    startTime = System.nanoTime();
+    timer.start();
     dualPivot.sort(backList2, 0, backList2.getSize() - 1);
-    endTime = System.nanoTime();
+    timer.stop();
     System.out.println("Backwards List - Sorted with Dual Pivot");
     backList2.display();
     System.out.print("Sorting time: ");
-    totalTime = endTime - startTime;
-    System.out.println(totalTime);
+    System.out.println(timer.totalTime);
     System.out.println();
+    timer.saveTime("backListDualPivot.txt");
 
     backList.saveList("backwardsArray.data");
 
