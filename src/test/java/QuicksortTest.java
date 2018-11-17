91ba2eebfc91567c307173890140543226e534cd
rooneyshuman <rooneyshuman@users.noreply.github.com>
2018-11-16 21:52:13 -0800

    Implemented correctness test for regular Quicksort

--- src/test/java/QuicksortTest.java
+++ src/test/java/QuicksortTest.java
@@ -1,3 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collections;
+
 public class QuicksortTest {
 
+  @Test
+  public void testQuicksortCorrectness_backList() {
+    Quicksort quickSort = new Quicksort();
+    int listSize = 1000;
+    List testList = new List(listSize);
+    List compList = new List(listSize);
+
+    //Load data into testing lists
+    testList.loadList("backList_1k.data");
+    compList.loadList("backList_1k.data");
+
+    //Library sort
+    Collections.sort(compList.getList());
+
+    //Project sort
+    quickSort.sort(testList, 0, listSize - 1);
+
+    //Assert lists are sorted equally
+    for (int i = 0; i < listSize; ++i) {
+      assertEquals(testList.getVal(i), compList.getVal(i));
+    }
+  }
 }
