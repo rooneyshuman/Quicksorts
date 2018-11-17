4999ff374f815ac3b9fc6046c3369e9946775217
rooneyshuman <rooneyshuman@users.noreply.github.com>
2018-11-16 22:05:24 -0800

    Added correctness test for QuickSort with almostSort1 list

--- src/test/java/QuicksortTest.java
+++ src/test/java/QuicksortTest.java
@@ -51,4 +51,27 @@ public class QuicksortTest {
       assertEquals(testList.getVal(i), compList.getVal(i));
     }
   }
+
+  @Test
+  public void testQuicksortCorrectness_almostSort1() {
+    Quicksort quickSort = new Quicksort();
+    int listSize = 1000;
+    List testList = new List(listSize);
+    List compList = new List(listSize);
+
+    //Load data into testing lists
+    testList.loadList("almostSort1_1k.data");
+    compList.loadList("almostSort1_1k.data");
+
+    //Library sort
+    Collections.sort(compList.getList());
+
+    //Project sort
+    quickSort.sort(testList, 0, listSize - 1);
+
+    //Assert lists are sorted equally
+    for (int i = 0; i < listSize; ++i) {
+      assertEquals(testList.getVal(i), compList.getVal(i));
+    }
+  }
 }
