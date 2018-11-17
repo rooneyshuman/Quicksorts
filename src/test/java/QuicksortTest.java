eee7e75f5903ba28e63b2d4411156d31bdb92427
rooneyshuman <rooneyshuman@users.noreply.github.com>
2018-11-16 22:02:18 -0800

    Added correctness testing for quicksort using random list

--- src/test/java/QuicksortTest.java
+++ src/test/java/QuicksortTest.java
@@ -28,4 +28,27 @@ public class QuicksortTest {
       assertEquals(testList.getVal(i), compList.getVal(i));
     }
   }
+
+  @Test
+  public void testQuicksortCorrectness_random() {
+    Quicksort quickSort = new Quicksort();
+    int listSize = 1000;
+    List testList = new List(listSize);
+    List compList = new List(listSize);
+
+    //Load data into testing lists
+    testList.loadList("random_1k.data");
+    compList.loadList("random_1k.data");
+
+    //Library sort
+    Collections.sort(compList.getList());
+
+    //Project sort
+    quickSort.sort(testList, 0, listSize - 1);
+
+    //Assert lists are sorted equally
+    for (int i = 0; i < listSize; ++i) {
+      assertEquals(testList.getVal(i), compList.getVal(i));
+    }
+  }
 }
