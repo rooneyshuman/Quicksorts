fb62dcb6b178882e3a48e4e61fe44b4eb0c0f1ed
rooneyshuman <rooneyshuman@users.noreply.github.com>
2018-11-20 21:27:09 -0800

    Added interface to facilitate testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,97 +1,129 @@
+import java.util.Scanner;
 
 public class Main {
+  private static Scanner sc = new Scanner(System.in);
+
   public static void main(String[] args) {
 
-    int listSize = 20;
-    Quicksort quick = new Quicksort();
-    QuickInsert insert = new QuickInsert();
-    Quick3 dualPivot = new Quick3();
+    boolean again = true;
+    int sort;
+    int list;
+    int size;
+
+    System.out.println("\n ----  TESTING INTERFACE ---- \n");
+    while (again) {
+
+      sort = sortMenu();
+      list = listMenu();
+      size = sizeMenu();
+
+      run(sort, list, size);
+
+      System.out.println("Again? y/n");
+      if (sc.next().equalsIgnoreCase("n"))
+        again = false;
+    }
+  }
+
+  private static int sortMenu() {
+    System.out.println("1. Quicksort");
+    System.out.println("2. QuickInsert");
+    System.out.println("3. Quick3");
+    System.out.println("4. Quick3Insert");
+
+    return sc.nextInt();
+  }
+
+  private static int listMenu() {
+    System.out.println("1. AlmostSort1");
+    System.out.println("2. AlmostSort2");
+    System.out.println("3. BackList");
+    System.out.println("4. Random List");
+
+    return sc.nextInt();
+  }
+
+  private static int sizeMenu() {
+    System.out.println("Enter the list size (1000, 10000, 100000, 1000000)");
+
+    return sc.nextInt();
+  }
+
+  private static String getListName(int list, int size) {
+
+    return (getListType(list).concat(getListSize(size)).concat(".data"));
+  }
+
+  private static String getListSize(int size) {
+    String suff = "";
+    if (size == 1000)
+      suff = "_1k";
+    else if (size == 10000)
+      suff = "_10k";
+    else if (size == 100000)
+      suff = "_100k";
+    else if (size == 1000000)
+      suff = "_1m";
+    return suff;
+  }
+
+  private static String getListType(int list) {
+    String pref = "";
+    if (list == 1)
+      pref = "almostSort1";
+    else if (list == 2)
+      pref = "almostSort2";
+    else if (list == 3)
+      pref = "backList";
+    else if (list == 4)
+      pref = "random";
+    return pref;
+  }
+
+  private static String getOutputFileName(int list, int size, int sort) {
+    String suff1 = "";
+
+    if (sort == 1)
+      suff1 = "_quick";
+    else if (sort == 2)
+      suff1 = "_quickIns";
+    else if (sort == 3)
+      suff1 = "_quick3";
+    else if (sort == 4)
+      suff1 = "_quick3Ins";
+
+    return (getListType(list).concat(getListSize(size)).concat(suff1).concat(".txt"));
+  }
+
+  private static void run(int sort, int list, int size) {
     Timer timer = new Timer();
+    QuickAbstract sortType;
+    String listFileName = getListName(list, size);
+    String outputFileName = getOutputFileName(list, size, sort);
+    if (sort == 1) {
+      System.out.println("Running: quicksort " + getListType(list) + " " + size);
+      sortType = new Quicksort();
+    } else if (sort == 2) {
+      System.out.println("Running: quickInsert " + getListType(list) + " " + size);
+      sortType = new QuickInsert();
+    } else if (sort == 3) {
+      System.out.println("Running: quick3 " + getListType(list) + " " + size);
+      sortType = new Quick3();
+    } else {
+      System.out.println("Running: quick3Insert " + getListType(list) + " " + size);
+      sortType = new Quick3Insert();
+    }
+    runTest(size, listFileName, outputFileName, sortType, timer);
+  }
 
-    List almost2 = new List(listSize);
-    almost2.almostSort2();
-    almost2.display();
-
-/*
-    // Backwards list sorted with regular quicksort
-    List backList = new List(listSize);
-    backList.backwardsList();         //generate backwards list
-    System.out.println("Backwards List - Unsorted");
-    backList.display();
-    timer.start();
-    quick.sort(backList, 0, backList.getSize() - 1);
-    timer.stop();
-    System.out.println("Backwards List - Sorted with Quick");
-    backList.display();
-    System.out.print("Sorting time: ");
-    System.out.println(timer.totalTime);
-    System.out.println();
-    timer.saveTime("backListQuick.txt");
-
-    // Random list sorted with quickInsert
-    List randomList = new List(listSize);
-    randomList.randomList();        //generate random list
-    System.out.println("Random List - Unsorted");
-    randomList.display();
-    timer.start();
-    insert.sort(randomList, 0, randomList.getSize() - 1);
-    timer.stop();
-    System.out.println("Random List - Sorted with QuickInsert");
-    randomList.display();
-    System.out.print("Sorting time: ");
-    System.out.println(timer.totalTime);
-    System.out.println();
-    timer.saveTime("randomListQuickInsert.txt");
-
-    // Almost sorted list sorted with quickInsert
-    List almostList = new List(listSize);
-    almostList.almostSort1(4);       //generate almost sorted list
-    System.out.println("Almost Sorted List - Unsorted");
-    almostList.display();
-    timer.start();
-    insert.sort(almostList, 0, almostList.getSize() - 1);
-    timer.stop();
-    System.out.println("Almost Sorted List - Sorted with QuickInsert");
-    almostList.display();
-    System.out.print("Sorting time: ");
-    System.out.println(timer.totalTime);
-    System.out.println();
-    timer.saveTime("almostSortedQuickInsert.txt");
-
-    //Backwards list sorted with dual pivot quicksort
-    List backList2 = new List(listSize);
-    backList2.backwardsList();         //generate backwards list
-    System.out.println("Backwards List - Unsorted");
-    backList2.display();
-    timer.start();
-    dualPivot.sort(backList2, 0, backList2.getSize() - 1);
-    timer.stop();
-    System.out.println("Backwards List - Sorted with Dual Pivot");
-    backList2.display();
-    System.out.print("Sorting time: ");
-    System.out.println(timer.totalTime);
-    System.out.println();
-    timer.saveTime("backListDualPivot.txt");
-
-    backList.saveList("backwardsArray.data");
-
-    List testList = new List(listSize);
-    testList.loadList("backwardsArray.data");
-    System.out.println("Loaded Backwards List");
-    testList.display();
-
-    Integer[] myArray = testList.convertToArray();
-*/
-/*
-    List randomList = new List(listSize);
-    randomList.randomList();
-    randomList.display();
-
-    List almostList = new List(listSize);
-    almostList.almostSort(4);
-    almostList.display();
-
-    */
-
+  private static void runTest(int size, String listFileName, String outputFileName, QuickAbstract sortType, Timer timer) {
+    for (int i = 0; i < 1000; ++i) {
+      List test = new List(size);
+      test.loadList(listFileName);
+      timer.start();
+      sortType.sort(test, 0, size - 1);
+      timer.stop();
+      timer.saveTime(outputFileName);
+    }
   }
 }
